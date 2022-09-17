91fd2a497640a73f9dac7243434b561012e1983e
vedant-jain03 <76901313+vedant-jain03@users.noreply.github.com>
2022-09-17 16:44:57 +0530

    feat: added page for registration

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ import './App.css';
 import MemePage from './pages/MemePage';
 import HomePage from './pages/HomePage';
 import RegisterPage from './pages/Register';
-import {Route, Routes} from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-    <Routes>
-    <Route exact path='/' element={<HomePage/>} />
-    <Route path='/memes' element={<MemePage/>} />
-    <Route path='/register' element={<RegisterPage/>} />
-    </Routes>
-    
+      <Routes>
+        <Route exact path='/' element={<HomePage />} />
+        <Route path='/memes' element={<MemePage />} />
+        <Route path='/register' element={<RegisterPage />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Navbar2.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import lightning from '../images/lightning.png';
+import logo from '../images/logo.png';
+import clouds from '../images/clouds.png';
+import '../style/navbar.css'
+function Navbar2() {
+    return (
+        <div className='navbar'>
+            <img src={lightning} className='lightning' />
+            <img src={logo} className='logo' />
+            <img src={lightning} className='lightning2' />
+        </div>
+    )
+}
+
+export default Navbar2
