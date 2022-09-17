f0d8a7e17f4b8483ab10c8db8de29ddf5e807a50
vedant-jain03 <76901313+vedant-jain03@users.noreply.github.com>
2022-09-17 23:36:19 +0530

    fix: add page title, icon and routing

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route exact path='/Meme-O-Meme' element={<HomePage />} />
-        <Route exact path='/Meme-O-Meme/memes' element={<MemePage />} />
-        <Route exact  path='/Meme-O-Meme/register' element={<RegisterPage />} />
+        <Route path='/' element={<HomePage />} />
+        <Route path='/memes' element={<MemePage />} />
+        <Route path='/register' element={<RegisterPage />} />
       </Routes>
     </div>
   );
