17ebceed8a1b1fb015a62760f36566dab7d72d57
Yashika Jotwani <yashikajotwani12@gmail.com>
2022-09-17 12:46:59 +0530

    Routing for registerPage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import MemePage from './pages/MemePage';
 import HomePage from './pages/HomePage';
+import RegisterPage from './pages/Register';
 import {Route, Routes} from 'react-router-dom';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <Routes>
     <Route exact path='/' element={<HomePage/>} />
     <Route path='/memes' element={<MemePage/>} />
+    <Route path='/register' element={<RegisterPage/>} />
     </Routes>
     
     </div>
---
+++ src/pages/Register.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+function RegisterPage() {
+  return (
+    <div>
+        <Navbar />
+    </div>
+  )
+}
+
+export default RegisterPage
