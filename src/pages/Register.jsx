012214c2f132fa082aafce51f6dbb17ee18c1582
vedant-jain03 <76901313+vedant-jain03@users.noreply.github.com>
2022-09-17 21:14:04 +0530

    feat: added firebase push notification

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import Navbar2 from '../components/Navbar2'
+import '../firebase.js'
 
 function RegisterPage() {
   const [interest, setInterest] = useState([
