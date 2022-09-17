d654a3cf352965852b7545eead9be45684a256b3
vedant-jain03 <76901313+vedant-jain03@users.noreply.github.com>
2022-09-17 10:45:20 +0530

    [ui] : Added home page and refactor files

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 import './App.css';
-import logo from './logo.png';
-import lightning from './lightning.png';
-import clouds from './clouds.png';
-import cat from './cat.png';
-import people from './people.png';
-import hello from './hi-hello-there.gif';
+import MemePage from './pages/MemePage';
+import HomePage from './pages/HomePage'
 
 function App() {
   return (
     <div className="App">
-      <div className='Home'>
-      <img className='lightning_logo' alt='LIGHTNING' src={lightning}/>
-      <img alt='LOGO' src={logo}/>
-      <img className='clouds_logo' alt='CLOUDS' src={clouds}/>
-      </div>
-      <div className='Introduction'>
-      <p className='Intro'>
-      <img className='hello' alt='HELLO' src={hello}/></p>
-      </div>
-      <div className='Footer'>
-      <img className='cat_logo' alt='cat' src={cat}></img>
-      <img className='people_logo' alt='people' src={people}></img>
-      </div>
+      <HomePage />
     </div>
   );
 }
