2bbc2e3caeaafe14d0d47c8214d31041190b423c
vedant-jain03 <76901313+vedant-jain03@users.noreply.github.com>
2022-09-18 10:09:19 +0530

    minor-fix: service-worker

--- src/index.js
+++ src/index.js
@@ -4,6 +4,17 @@ import './index.css';
 import App from './App';
 import { HashRouter } from 'react-router-dom';
 
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("./firebase-messaging-sw.js")
+    .then(function(registration) {
+      console.log("Registration successful, scope is:", registration.scope);
+    })
+    .catch(function(err) {
+      console.log("Service worker registration failed, error:", err);
+    });
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
