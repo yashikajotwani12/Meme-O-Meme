b07f61bd13b15e0957e9d969860715dcd97e1466
Yashika Jotwani <yashikajotwani12@gmail.com>
2022-09-18 11:07:03 +0530

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,7 +1,6 @@
-/* eslint-disable no-undef */
-importScripts('https://www.gstatic.com/firebasejs/8.6.8/firebase-app.js')
-importScripts('https://www.gstatic.com/firebasejs/8.6.8/firebase-messaging.js')
 
+importScripts("https://www.gstatic.com/firebasejs/5.9.4/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/5.9.4/firebase-messaging.js");
 const firebaseConfig = {
     apiKey: "AIzaSyCvZYqnlVMtmDJjSQ_WNQa6nFXul9z46mk",
     authDomain: "login-f4489.firebaseapp.com",
@@ -12,25 +11,25 @@ const firebaseConfig = {
     appId: "1:564447251099:web:1436d380f85b660590143e",
     measurementId: "G-7G4D0VS2Q1"
 };
-
-firebase.initializeApp(firebaseConfig)
-
-const messaging = firebase.messaging()
-
-messaging.onBackgroundMessage((payload) => {
-    console.log('[firebase-messaging-sw.js] Received background message ', payload)
-    const notificationTitle = payload.notification.title
-    const notificationOptions = {
-        body: payload.notification.body,
-        icon: payload.notification.icon || payload.notification.image,
-    }
-
-    self.registration.showNotification(notificationTitle, notificationOptions)
-})
-
-self.addEventListener('notificationclick', (event) => {
-    if (event.action) {
-        clients.openWindow(event.action)
-    }
-    event.notification.close()
-})
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+messaging.setBackgroundMessageHandler(function (payload) {
+    const promiseChain = clients
+        .matchAll({
+            type: "window",
+            includeUncontrolled: true,
+        })
+        .then((windowClients) => {
+            for (let i = 0; i < windowClients.length; i++) {
+                const windowClient = windowClients[i];
+                windowClient.postMessage(payload);
+            }
+        })
+        .then(() => {
+            return registration.showNotification("my notification title");
+        });
+    return promiseChain;
+});
+self.addEventListener("notificationclick", function (event) {
+    console.log(event);
+});
